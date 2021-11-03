fb7ed759926908dce7fbc638e9e4d296816957eb
nagasaki <76617420+qiuyueluzi@users.noreply.github.com>
2021-11-03 14:34:58 +0900

    Update draw_graph.js

--- compound_graph/draw_graph.js
+++ compound_graph/draw_graph.js
@@ -68,7 +68,7 @@ $(function(){
             for(var x=0; x<classification["mml_classification"].length; x++){
                 var parents = classification.mml_classification[x]["directory"].split('/')
                 for(var y = parents.length - 1; y > 0; y--){
-                    console.log(parents[y])
+                    console.log(cy.$(parents[y]), cy.$('#' + parents[y]))
                     if(cy.$(parents[y]) == undefined){
                         if(y > 1)cy.add({group: 'nodes', data:{id: parents[y], parent: parents[y-1]}})
                         else cy.add({group: 'nodes', data: {id: parents[y]}})
