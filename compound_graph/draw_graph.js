4f9e188b04bd49b06014065b95c5af9f9e30b872
nagasaki <76617420+qiuyueluzi@users.noreply.github.com>
2021-11-04 16:43:05 +0900

    Update draw_graph.js

--- compound_graph/draw_graph.js
+++ compound_graph/draw_graph.js
@@ -53,7 +53,7 @@ $(function(){
             cy.add(dot_graph["eleObjs"]);
             // Set graph style
             var layout = cy.elements().layout({
-                name: 'preset',
+                name: 'breadthfirst',
                 //animate: false
             })
             layout.run()
