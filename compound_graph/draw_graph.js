943fe3edacd4c16a43054cd616a6823a58258ad3
nagasaki <76617420+qiuyueluzi@users.noreply.github.com>
2021-11-04 16:41:35 +0900

    Update draw_graph.js

--- compound_graph/draw_graph.js
+++ compound_graph/draw_graph.js
@@ -53,7 +53,7 @@ $(function(){
             cy.add(dot_graph["eleObjs"]);
             // Set graph style
             var layout = cy.elements().layout({
-                name: 'dagre',
+                name: 'preset',
                 //animate: false
             })
             layout.run()
