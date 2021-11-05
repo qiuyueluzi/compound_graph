8ac7ef4813f626ed3d821bd03a19bb105abc9d8d
nagasaki <76617420+qiuyueluzi@users.noreply.github.com>
2021-11-05 15:45:23 +0900

    Update draw_graph.js

--- compound_graph/draw_graph.js
+++ compound_graph/draw_graph.js
@@ -304,7 +304,7 @@ $(function(){
         var doubleClickDelayMs= 350; //ダブルクリックと認識するクリック間隔
         var previousTapStamp = 0;
         cy.nodes().on('tap', function(e) {
-            if(childrenData.get(e.target.id()).node > 0){//複合親ノードであればダブルクリックかを判定
+            if(childrenData.get(e.target.id()).node.length > 0){//複合親ノードであればダブルクリックかを判定
                 var currentTapStamp= e.timeStamp;
                 var msFromLastTap= currentTapStamp -previousTapStamp;
                 if (msFromLastTap < doubleClickDelayMs) {
