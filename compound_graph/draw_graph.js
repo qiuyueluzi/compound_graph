da13db7397360019439c7bd548cdc5763d2aa96d
nagasaki <76617420+qiuyueluzi@users.noreply.github.com>
2021-12-01 16:41:24 +0900

    Update draw_graph.js

--- compound_graph/draw_graph.js
+++ compound_graph/draw_graph.js
@@ -386,7 +386,7 @@ $(function(){
         });
 
         $("#close").click(function(){
-            $("#open").css('background-color', 'rgba(255, 255, 255, 0)')
+            $("#open").css('background-color', 'default')
             let bottom = -1;
             let removes = [];
             nodes.parent().forEach(function(node){
@@ -404,7 +404,7 @@ $(function(){
             if(bottom == 0)$("#close").css('background-color', 'gray')
         })
         $("#open").click(function(){
-            $("#close").css('background-color', 'rgba(255, 255, 255, 0)')
+            $("#close").css('background-color', 'default')
             cy.nodes().forEach(function(node){
                 if(childrenData.get(node.id()).removed && childrenData.get(node.id()).node.length) restoreChildren(node.id(), node, childrenData, edgesData)
             })
