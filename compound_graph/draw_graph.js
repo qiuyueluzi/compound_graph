025382a06b55a2634afcbc617e1290ffe75d5463
nagasaki <76617420+qiuyueluzi@users.noreply.github.com>
2021-12-02 16:59:20 +0900

    Update draw_graph.js

--- compound_graph/draw_graph.js
+++ compound_graph/draw_graph.js
@@ -340,8 +340,8 @@ $(function(){
 
         // ノードをクリックした場合、リンクに飛ぶ(htmlリンクの設定)
         cy.nodes().on("cxttap", function(event){
-            if(childrenData.get(event).node.length > 0){
-                e.target.trigger('doubleTap', e);
+            if(childrenData.get(this).node.length > 0){
+                event.target.trigger('doubleTap', event);
             }
             else{
                 try {  // your browser may block popups
