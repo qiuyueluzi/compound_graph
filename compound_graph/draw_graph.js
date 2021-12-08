22401f030bffddd08c8c7e71bff8418d2c45c4df
qiuyueluzi <i045ff@yamaguchi-u.ac.jp>
2021-12-08 13:00:37 +0900

    update

--- compound_graph/draw_graph.js
+++ compound_graph/draw_graph.js
@@ -332,7 +332,7 @@ $(function(){
         
         // ノードの上にカーソルが来たとき，ノード名を表示する
         $(window).on("mousemove", function(window_event){ 
-            /*cy.nodes().on("mouseover", function(cy_event){
+            cy.nodes().on("mouseover", function(cy_event){
                 document.getElementById("name-plate").style.top = window_event.clientY + (10) + "px";
                 document.getElementById("name-plate").style.left = window_event.clientX + (10) +"px";
                 if(childrenData.get(cy_event.target.data("id")).node.length > 0 && childrenData.get(cy_event.target.data("id")).removed){
@@ -357,7 +357,7 @@ $(function(){
                 }
                 else document.getElementById("name-plate").innerHTML = cy_event.target.data("name");
             });
-            cy.nodes().on("mouseout", function(){
+            /*cy.nodes().on("mouseout", function(){
                 document.getElementById("name-plate").style.fontSize = ""
                 document.getElementById("name-plate").innerHTML = "";
             })*/
