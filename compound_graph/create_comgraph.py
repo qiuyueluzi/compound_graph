456a02a86da546913150d635f0980ba3046b40a9
qiuyueluzi <i045ff@yamaguchi-u.ac.jp>
2022-02-21 16:05:52 +0900

    py

--- compound_graph/create_comgraph.py
+++ compound_graph/create_comgraph.py
@@ -1,7 +1,4 @@
 import json
-import pandas
 
 graph_json = open('graph_attrs/compound_dot_graph.json', 'r')
 graph_objects = json.load(graph_json)
-
-classfication = pandas.read_excel('graph_attrs/mml_classification')
