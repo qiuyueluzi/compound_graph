b1ba6d141f275e16842147cbacb06ddeb5470b57
qiuyueluzi <i045ff@yamaguchi-u.ac.jp>
2022-02-22 19:36:36 +0900

    json

--- compound_graph/create_comgraph.py
+++ compound_graph/create_comgraph.py
@@ -1,4 +1,9 @@
 import json
 
 graph_json = open('graph_attrs/compound_dot_graph.json', 'r')
+class_json = open('graph_attrs/mml_classification.json', 'r')
 graph_objects = json.load(graph_json)
+class_objects = json.load(class_json)
+
+print(graph_objects)
+print(class_objects)
