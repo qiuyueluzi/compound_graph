2fed07545fa8de75c297547d773fddc782238861
qiuyueluzi <i045ff@yamaguchi-u.ac.jp>
2022-05-19 17:49:00 +0900

    createJson

--- compound_graph/create_comgraph.py
+++ compound_graph/create_comgraph.py
@@ -5,5 +5,12 @@ class_json = open('graph_attrs/mml_classification.json', 'r')
 graph_objects = json.load(graph_json)
 class_objects = json.load(class_json)
 
-print(graph_objects)
-print(class_objects)
+
+
+for index in range(334):
+    if graph_objects['eleObjs'][index]['data']['id'].lower() == class_objects['mml_classification'][index]['mml-name']:
+        graph_objects['eleObjs'][index].update(class_objects['mml_classification'][index])
+        
+
+with open('graph_attrs/graph_class.json', 'w') as f :
+    json.dump(graph_objects, f, indent=4)
